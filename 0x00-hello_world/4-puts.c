cb9fa56b669836769842e80f6e5c400293b65908
Opeoluwa <adeyeriopeoluwa05@gmail.com>
2022-09-08 04:06:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /* standard input output */
-int main (void){
+int main(void){
 	/* Main function */
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts('"',"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
