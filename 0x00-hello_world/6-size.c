0c2e497a04b8f2ea3b60d3f28d5d7accf046f3e3
Opeoluwa <adeyeriopeoluwa05@gmail.com>
2022-09-08 06:13:32 -0700

    puts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - The main function
+* Return: 0 if successful. 1 otherwise
+*/
+int main(void)
+{
+	printf("Size of char: %2d byte(s)\n");
+	printf("size of int: %2d byte(s)");
+
