6057935bf2336aa6f4bd02c453317bf50e03cfa2
Opeoluwa <adeyeriopeoluwa05@gmail.com>
2022-09-08 04:48:43 -0700

    puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - The main function
+* Return: 0 for successful. 1 otherwise
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
