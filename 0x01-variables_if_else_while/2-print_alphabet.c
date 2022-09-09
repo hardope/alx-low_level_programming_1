a1cf058a0ec880498b2317c471931e46c9e9d2b8
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-09 15:28:35 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
 /* more headers goes there */
 /* Initial short description */
 /* betty style doc for function main goes there */
@@ -10,18 +9,9 @@
 */
 int main(void)
 {
-	int n;
-	int m;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	m = n % 10;
-	if (m == 0)
-		printf("Last digit of %i is 0 and is 0\n", n);
-	if (m > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, m);
-	if (m < 6 && m != 0)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, m);
+	int a;
+	for (a = 97; a < 122, a++)
+		putchar("%c", a);
+	putchar("/n");
 	return (0);
 }
