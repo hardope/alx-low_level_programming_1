c9ab2fb686b7c75d62fea15c833af7820780e9f5
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-09 23:49:31 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,10 @@
 #include <stdio.h> 
- /** 
-  * main - entry point 
-  * 
-  * Return: alway returns 0 
-  * 
-  */ 
+/** 
+ * main - entry point 
+ * 
+ * Return: alway returns 0 
+ * 
+ */ 
  int main(void) 
  { 
          int i; 
