372320b321e6986d67a608854f0108c7b91513a3
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-09 23:46:42 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-/* more headers goes there */
-/* Initial short description */
-/* betty style doc for function main goes there */
-/**
-* main - This is a description
-* Return: 0 if successful. 1 otherwise
-*/
-int main(void)
-{
-	int a = 48;
-	int b = 44;
-	char arr[] = {01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48, 49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89}
-
-	while (a < 58)
-	{
-		putchar(a);
-		if (a != 57)
-		{
-			putchar(b);
-			putchar(32);
-		}
-		a++;
-	}
-	putchar('\n');
-	return (0);
-}
+#include <stdio.h> 
+ /** 
+  * main - entry point 
+  * 
+  * Return: alway returns 0 
+  * 
+  */ 
+ int main(void) 
+ { 
+         int i; 
+         int j; 
+  
+         for (i = 10; i <= 19; i++) 
+         { 
+                 for (j = 10; j <= 19; j++) 
+                 { 
+                         if ((j % 10) > (i % 10)) 
+                         { 
+                                 putchar((i % 10) + '0'); 
+                                 putchar((j % 10) + '0'); 
+                                 if (i != 18 || j != 19) 
+                                 { 
+                                         putchar(','); 
+                                         putchar(' '); 
+                                 } 
+                         } 
+  
+                 } 
+         } 
+         putchar('\n'); 
+         return (0); 
+ }
