6a39c72eabf077cb7b3adad7c14f5ac94201b161
Opeoluwa <adeyeriopeoluwa05@gmail.com>
2022-09-09 07:44:32 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+/* more headers goes there */
+/* Initial short description */
+/* betty style doc for function main goes there */
+/**
+* main - This is a description
+* Return: 0 if successful. 1 otherwise
+*/
+int main(void)
+{
+	char b;
+
+	int a = 97;
+
+	while (a < 123)
+	{
+		b = (char)(a);
+		putchar(b);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
