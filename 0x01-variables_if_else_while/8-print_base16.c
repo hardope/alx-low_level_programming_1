e58664b81a633945af232484f101a66118d35ca4
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-09 16:36:59 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,18 @@
 */
 int main(void)
 {
-	int a = 0;
+	int a = 48;
+	int b = 97;
 
-	while (a < 10)
+	while (a < 58)
 	{
-		printf("%i", a);
+		putchar(a);
 		a++;
+	}
+	while (b < 103)
+	{
+		putchar(b);
+		b++;
 	}
 	putchar('\n');
 	return (0);
