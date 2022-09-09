35b6d328ecfda6ea0010bf600eac4ef63197b2ce
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-09 13:03:55 +0100

    Update 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+  sprintf(text, "%d", n);
+  int last_digit = int(text[strlen(text -1)])
+  
+  if last_digit == 0
+    printf("%i is zero\n", n);
+  else if last_digit < 0
+    printf("%i is negative\n", n);
+  else
+    printf("%i is positive\n", n);
+
+	return (0);
+}
