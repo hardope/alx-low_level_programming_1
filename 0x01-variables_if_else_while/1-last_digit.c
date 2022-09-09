d6098d55f5f58139658a0f1ff9c2f0da8d86fd1c
Opeoluwa <adeyeriopeoluwa05@gmail.com>
2022-09-09 06:35:55 -0700

    lats_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/* more headers goes there */
+/* Initial short description */
+/* betty style doc for function main goes there */
+/**
+* main - This is a description
+* Return: 0 if successful. 1 otherwise
+*/
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	while (n >= 10)
+	{
+		n = n / 10
+	}
+	if (last == 0)
+		printf("%i is zero\n", n);
+	if (last > 0)
+		printf("%i is positive\n", n);
+	if (last < 0)
+		printf("%i is negative\n", n);
+	return (0);
+}
