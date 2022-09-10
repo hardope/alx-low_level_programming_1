712416c7981f680511f0726baac413fe9f3d616f
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-10 10:58:14 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,32 +1,31 @@
-#include <stdio.h> 
-/** 
- * main - entry point 
- * 
- * Return: alway returns 0 
- * 
- */ 
- int main(void) 
- { 
-         int i; 
-         int j; 
-  
-         for (i = 10; i <= 19; i++) 
-         { 
-                 for (j = 10; j <= 19; j++) 
-                 { 
-                         if ((j % 10) > (i % 10)) 
-                         { 
-                                 putchar((i % 10) + '0'); 
-                                 putchar((j % 10) + '0'); 
-                                 if (i != 18 || j != 19) 
-                                 { 
-                                         putchar(','); 
-                                         putchar(' '); 
-                                 } 
-                         } 
-  
-                 } 
-         } 
-         putchar('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entyr point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c, i;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
