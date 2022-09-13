6f97f242bba94c367e19318b2c32a301669f209d
Opeoluwa <adeyeriopeoluwa05@gmail.com>
2022-09-13 14:06:01 -0700

    advanced

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*  print_sign - Print
+* @n: Value of parameter
+* Return: Values
+*/
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
+}
