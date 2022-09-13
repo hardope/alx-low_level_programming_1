2d5d6f51429cc5d999c9b4781b5cfda007fa9dfa
Opeoluwa <adeyeriopeoluwa05@gmail.com>
2022-09-13 03:01:33 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stdio.h>
+int _islower(int c);
+/**
+* main - Print
+* Return: 0. Successful.
+*/
+int main(void)
+{
+	int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
+/**
+*  _islower - Print
+*/
+int _islower(int c)
+{
+	return (islower(c));
+}
