d3c499ffd8ae988ac25d838297278240b7f43380
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-13 15:25:16 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,12 +7,12 @@
 int print_sign(int n)
 {
 	if (n < 0)
-		putchar('-')
+		putchar('-');
 		return(-1);
 	else if (n == 0)
-		putchar('0')
+		putchar('0');
 		return (0);
 	else
-		putchar('+')
+		putchar('+');
 		return(1);
 }
