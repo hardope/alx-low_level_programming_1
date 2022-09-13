6b13d4d7d5b56b0bcbc508abd883401966e1eba5
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-13 20:40:45 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,16 @@
 #include "main.h"
 # include<stdio.h>
 /**
-*  _abs - Print
+*  print_to_98 - Print
 * @a: Value of parameter
 * Return: Values
 */
 void print_to_98(int n)
 {
 	int i;
-	for (i = n; i < 98, i++)
+	for (i = n; i < 98; i++)
 	{
-		printf('%i', i);
+		printf("%i", i);
 	}
-	printf('\n');
+	printf("\n");
 }
