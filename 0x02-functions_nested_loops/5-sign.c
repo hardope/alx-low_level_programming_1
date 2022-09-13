bf1a87946640d9af1efb4e1b59798dfcca91ee57
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-13 11:28:53 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,48 @@
 #include "main.h"
 #include<stdio.h>
-int _islower(int c);
+int print_sign(int n);
 /**
 * main - Print
 * Return: 0. Successful.
 */
 int main(void)
 {
-	int r;
+    int r;
 
-    r = _islower('H');
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
     _putchar(r + '0');
-    r = _islower('o');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
     _putchar(r + '0');
-    r = _islower(108);
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
     _putchar(r + '0');
     _putchar('\n');
     return (0);
 }
 /**
-*  _islower - Print
+*  _print_sign - Print
 */
-int _islower(int c)
+int print_sign(int n)
 {
-	return (islower(c));
+	if (n < 0)
+		putchar('-')
+		return(-1);
+	else if (n == 0)
+		putchar('0')
+		return (0);
+	else
+		putchar('+')
+		return(1);
 }
