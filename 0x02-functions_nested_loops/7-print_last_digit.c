7ff9e2c411a2cac9441e28169a9a65dbfbd3ddce
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-13 15:46:04 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,10 @@
 #include "main.h"
 /**
 *  print_sign - Print
-* @n: Value of parameter
 * Return: Values
 */
-int print_sign(int n)
+int print_last_digit(int)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('+');
-		return (1);
-	}
+	int last = int % 10;
+	return (last)
 }
