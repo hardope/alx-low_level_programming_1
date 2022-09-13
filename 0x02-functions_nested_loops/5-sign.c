2555466030fe8f1dbb756e7f7ab00a17d4ecbba8
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-13 15:23:56 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,38 +1,8 @@
 #include "main.h"
-#include<stdio.h>
-int print_sign(int n);
-/**
-* main - Print
-* Return: 0. Successful.
-*/
-int main(void)
-{
-    int r;
-
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
-}
 /**
 *  _print_sign - Print
+* @n: Value of parameter
+* Return: Values
 */
 int print_sign(int n)
 {
