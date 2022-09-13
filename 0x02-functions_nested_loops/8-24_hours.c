bc47dd43ec6f432046d84535de7cadaffceff304
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-13 18:56:34 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,11 @@ void jack_bauer(void)
 	int l;
 
 	int count = 0;
-	for (i = 48; i < 51; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (j = 48; j < 57; j++)
+		for (j = 48; j < 58; j++)
 		{
-			if (count > 1300)
+			if (count > 1390)
 			{
 				break;
 			}
