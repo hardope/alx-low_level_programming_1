3066212bc938132f4a8aecb7d3054941033cd183
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-13 17:05:56 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,13 @@ int print_last_digit(int a)
 {
 	int last = a % 10;
 
+	a = (int)(a)
 	if (a < 0)
 		a = (-1) * a;
 	if ((a + '0')  == ',')
-		a = 4;
+		a = 52;
 	if ((a + '0') == '(')
-		a = 8;
+		a = 56;
 
 	_putchar(last + '0');
 	return (last);
