6cca455a86ab9a8b9496caf67551671717987416
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-13 14:47:46 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,9 @@ void print_alphabet_x10(void)
 {
 	int a;
 	int count;
-	
-	count = 0;
+
 	a = 97;
-	while (count < 10)
+	for (count = 0; count < 10; count++)
 	{
 		while (a < 122)
 		{
@@ -17,6 +16,5 @@ void print_alphabet_x10(void)
 			a++;
 		}
 		_putchar('\n');
-		count++;
 	}
 }
