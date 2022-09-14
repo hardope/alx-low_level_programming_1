bec82ed8d30c2d72ccf3df807193a2ed2839bdfc
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-14 05:26:59 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	int start_num, end_num, total;
+
 	end_num = 1024;
 	total = 0;
-
 	for (start_num = 0; start_num < end_num; start_num++)
 	{
 		if ((start_num % 3 == 0) || (start_num % 5 == 0))
