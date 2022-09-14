4b30708e074af3e45a55e316fe642040171c1782
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-14 05:37:42 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,26 +6,26 @@
  */
 int main(void)
 {
-	int counter = 2;
+int counter = 2;
 
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
+float a = 1;
+float b = a + 1;
+float c = a + b;
 
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter < 98)
-	{
-		counter++;
-		printf("%.0f", c);
-		a = b;
-		b = c;
-		c = a + b;
-		if (counter < 98)
-		{
-			printf(", ");
-		}
-	}
-	printf("\n");
-	return (0);
+printf("%.0f, ", a);
+printf("%.0f, ", b);
+while (counter < 98)
+{
+counter++;
+printf("%.0f", c);
+a = b;
+b = c;
+c = a + b;
+if (counter < 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+return (0);
 }
