871346a43d37312a952d38a1165e1087c3a370fe
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-14 18:36:43 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,6 @@
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar\n);
 	return (0);
 }
