4aa72ff1b6adfa894499fce73bc28d00de74e1b3
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-14 20:43:15 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,11 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
-    else if (b > a && b > c)
+    if (b > a && b > c)
     {
         largest = b;
     }
-    else
+    if (c > a && c > b)
     {
         largest = c;
     }
