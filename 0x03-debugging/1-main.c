f88ae2ab5aa3f9f4bcddbf4613cb20a528c309b3
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-14 14:52:07 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,6 @@ int main(void)
         int i;
 
         printf("Infinite loop incoming :(\n");
-
         i = 0;
 
 /**     while (i < 10) 
@@ -18,6 +17,5 @@ int main(void)
 */
 
         printf("Infinite loop avoided! \\o/\n");
-
         return (0);
 }
