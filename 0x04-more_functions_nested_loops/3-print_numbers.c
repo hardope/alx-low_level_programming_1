d7a48a37daf03eb72e2c2a0c831858ff6470b543
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 07:06:41 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,10 @@
-#include<stdio.h>
-int main(void)
-
+#include "main.h"
+/**
+*  print_numbers - Print
+*/
+void print_numbers(void)
+{
+    for (i = 48; i < 58; i++)
+        _putchar(i);
+    _putchar('\n');
+}
