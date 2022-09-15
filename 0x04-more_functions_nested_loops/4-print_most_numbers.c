086b6702c99332eeef6afa06ae32bfa66bad3149
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 08:26:52 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,12 +4,12 @@
 */
 void print_most_numbers(void)
 {
-    for (i = 48; i < 58; i++)
-    {
-        if (i == 50 || i == 52)
-            continue;
-        else
-            _putchar(i);
-    }
-    _putchar('\n');
+        for (i = 48; i < 58; i++)
+        {
+                if (i == 50 || i == 52)
+                        continue;
+                else
+                        _putchar(i);
+        }
+        _putchar('\n');
 }
