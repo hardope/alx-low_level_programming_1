150d31c967fae0920bb7cefc33751b19cc23dc18
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 14:58:47 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,11 +6,11 @@
  */
 void print_line(int n)
 {
-			  int i;
+	int i;
 
-        for (i = 0; i < n; i++)
-        {
-          _putchar('_');
-        }
-        _putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
