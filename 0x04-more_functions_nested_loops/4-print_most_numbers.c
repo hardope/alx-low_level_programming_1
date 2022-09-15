0accda2c336cc8343de77647e1925d5bb6a1004d
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 07:10:24 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,3 +1,15 @@
-#include<stdio.h>
-int main(void)
-
+#include "main.h"
+/**
+*  print_most_numbers - Print
+*/
+void print_most_numbers(void)
+{
+    for (i = 48; i < 58; i++)
+    {
+        if (i == 50 || i == 52)
+            continue;
+        else
+            _putchar(i);
+    }
+    _putchar('\n');
+}
