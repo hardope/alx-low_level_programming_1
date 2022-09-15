4f5a8700cdc04fb5ee1c381d5db6f181c9cbd98f
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 15:08:27 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,18 +5,20 @@
 */
 void print_square(int size)
 {
-	int i;
-	int j;
+	int i, j;
 
-	if (size > 0)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
 	{
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
+			{
 				_putchar('#');
-			_putchar('\n);
+			}
+			_putchar('\n');
 		}
 	}
-	else
-		_putchar('\n);
 }
