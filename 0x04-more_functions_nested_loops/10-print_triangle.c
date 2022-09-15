201cf9c368c807cdf3de7f0d3d0d3ea445c938d0
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 15:18:31 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,9 +7,9 @@ void print_triangle(int size)
 {
         int i, j, k;
 
-        for (i = 0; i < size; i++)
+        for (i = 0; i <= size; i++)
 	{
-		for (j = size; j > i; j--)
+		for (j = size - 1; j > i; j--)
 		{
 			_putchar(' ');
 		}
