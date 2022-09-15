2e5fa0eac42cd8e77cd43c3aaa4f9422fd224ee4
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 13:01:31 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,23 +4,23 @@
 */
 void more_numbers(void)
 {
-	int i, j;
+int i, j;
 
-	for (j = 0; j < 10; j++)
-	{
-		for (i = 0; i < 15; i++)
-		{
-		       if (i > 9)
-		       {
-				_putchar('1');
-				_putchar((i-10) + '0');
-		       }
-			else
-			{
-                   		_putchar(i + '0');
-			}
-		 }
-		_putchar('\n');
-	}
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 15; i++)
+{
+if (i > 9)
+{
+_putchar('1');
+_putchar((i-10) + '0');
+}
+else
+{
+_putchar(i + '0');
+}
+}
+_putchar('\n');
+}
         
 }
