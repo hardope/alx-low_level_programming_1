16122cab4643d67fec953cffb82935ec310b4196
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 08:25:54 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,8 +6,8 @@
 */
 int _isdigit(int c)
 {
-    if (c >= 48 && c <= 58)
-        return (1);
-    else
-        return (0);
+        if (c >= 48 && c <= 58)
+                return (1);
+        else
+                return (0);
 }
