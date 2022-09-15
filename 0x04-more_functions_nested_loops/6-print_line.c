d282ad997d6099897b7b15018aad6eaab61da7bb
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 14:57:29 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
-*  print_line - Print
-* @n: Value
-*/
+ * print_line - prints a line
+ *
+ * @n: integer to print stuff?
+ */
 void print_line(int n)
 {
-int i;
+			  int i;
 
-if (n > 0)
-{
-for (i = 0; i < n; i ++)
-_putchar(95);
-_putchar('\n')
-}
-else
-_putchar('\n');
+        for (i = 0; i < n; i++)
+        {
+          _putchar('_');
+        }
+        _putchar('\n');
 }
