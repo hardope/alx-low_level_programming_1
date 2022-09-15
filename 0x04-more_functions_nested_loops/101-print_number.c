d04a0d77d06181fe42e67b352d1f94aa909eb343
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 19:13:17 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -44,3 +44,14 @@ void print_number(int n)
 		d = d / 10;
 	}
 }
+	for (i = 1; i < c; i++)
+	{
+		d *= 10;
+	}
+	for (i = 0; i < c; i++)
+	{
+		_putchar(y / d + '0');
+		y = y % d;
+		d = d / 10;
+	}
+}
