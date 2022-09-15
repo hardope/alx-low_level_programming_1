73c1e5d270a6fdaba4b5b92174ed78723637c9d6
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 15:13:04 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,17 +6,25 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i < 3 || i == 4)
-			printf("%i ", i);
-		else if ((i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz ");
-		else if ((i % 3) == 0)
-			printf("Fizz ");
-		else if ((i % 5) == 0)
-			printf("Buzz ");
-		else
-			printf("%i ", i);
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+			printf(" ");
 	}
+	printf("\n");
+	return (0);
 }
