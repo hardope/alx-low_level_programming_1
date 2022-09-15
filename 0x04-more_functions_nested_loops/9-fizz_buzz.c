1f4e0f2a2ebc4ca4386594c92faf4d858a0ca57f
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 09:30:27 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
+/**
+* main: Main function
+*/
 int main(void)
 {
-	here
+	for (int i = 1; i < 100; i++)
+	{
+		if (i < 3 || i == 4)
+			printf("%i ", i);
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%i ", i);
+	}
 }
