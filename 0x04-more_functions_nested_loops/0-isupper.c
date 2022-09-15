7a4d561cc15fbb60f9e52c2b6a74c1d9434f865f
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 06:44:22 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,13 @@
-#include<stdio.h>
-int main(void)
-
+#include "main.h"
+/**
+*  _isalpha - Print
+* @c: Description for c
+* Return: 0. Success
+*/
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+	else
+		return (0);
+}
