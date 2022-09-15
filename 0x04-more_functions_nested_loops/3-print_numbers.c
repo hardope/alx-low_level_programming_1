69f2011b6ed7d9369cff071552fa924a4727f98c
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 12:36:30 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,9 +4,11 @@
 */
 void print_numbers(void)
 {
-    int i;
+int i;
 
-    for (i = 48; i < 58; i++)
-            _putchar(i);
-    _putchar('\n');
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
