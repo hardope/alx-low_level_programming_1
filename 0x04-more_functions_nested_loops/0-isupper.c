9737c94fe440ce0aec756d70887dd7537c52529e
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 12:24:17 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,8 +6,8 @@
 */
 int _isupper(int c)
 {
-if ((c > 64 && c < 91)
-return (1);
-else
-return (0);
+        if ((c > 64 && c < 91)
+                return (1);
+        else
+                return (0);
 }
