13ce65d4a301f172a18237075013fbbfe46b99d3
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 08:23:20 +0100

    Update 5-print_more_numbers.c

--- 0x04-more_functions_nested_loops/5-print_more_numbers.c
+++ 0x04-more_functions_nested_loops/5-print_more_numbers.c
@@ -1,5 +1,16 @@
-#include<stdio.h>
-int main(void)
+#include "main.h"
+/**
+*  more_numbers - Print
+*/
+void more_numbers(void)
 {
-	here
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+        
 }
