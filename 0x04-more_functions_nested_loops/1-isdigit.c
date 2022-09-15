a6113e1f7a60c8db058107ab6792e93ae90f9026
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-15 06:57:24 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,13 @@
-
+#include "main.h"
+/**
+*  _isalpha - Print
+* @c: Description for c
+* Return: 0. Success
+*/
+int _isupper(int c)
+{
+    if ((c > 64 && c < 91)
+        return (1);
+    else
+        return (0);
+}
